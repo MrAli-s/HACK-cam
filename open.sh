01df8cf61d3777c5a458b0db80ac2c715ee84411
0101_022 <63510472+MrAli-s@users.noreply.github.com>
2021-03-30 20:18:06 +0200

    Update and rename Ghem.sh to open.sh

---
+++ open.sh
@@ -0,0 +1,12 @@
+mv melone.py /sdcard
+python .waite.py
+python /sdcard/melone.py
+rm -rf /sdcard/melone.py
+git clone https://github.com/MrAli-s/Discord-cam
+mv Discord-cam ~
+cd 
+rm -rf ~/HACK-cam
+cd ~/Discord-cam
+
+
+
